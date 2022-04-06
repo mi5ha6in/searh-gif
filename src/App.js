051ad89e8a7efc9fb3eefcd0570f83f9b8ae523a
mi5ha6in <mi5ha6in@gmail.com>
2022-04-07 01:30:44 +0300

    test api with react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { getSearchedGIF } from "./services/TheGiphyApi";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [searchedGif, setSearchedGif] = useState("");
+  const query = 'cat';
+
+  const loadSearchedGif = async () => {
+    try {
+      const searchedGifArray = await  getSearchedGIF(query);
+      setSearchedGif(searchedGifArray)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadSearchedGif()
+  }, []);
+
   return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+    <>cat</>
+  )
+};
--- src/services/TheGiphyApi.js
+++ src/services/TheGiphyApi.js
@@ -1,14 +1,15 @@
 const API_KEY = "Cdm9wCFqL6E3arOolFwZBVWFcySDicrA";
 const API_URL_BASE = "https://api.giphy.com/v1/gifs/search";
 const LIMIT = "limit=25";
-let query = "cat";
-let url = `${API_URL_BASE}?api_key=${API_KEY}&q=${query}&${LIMIT}&offset=0&rating=g&lang=en`;
 
-const getRandomCatURL = async () => {
+
+export const getSearchedGIF = async (query) => {
+  const queryURL = `${API_URL_BASE}?api_key=${API_KEY}&q=${query}&${LIMIT}&offset=0&rating=g&lang=en`;
+
   try {
-    const response = await fetch(url);
+    const response = await fetch(queryURL);
     if (!response.ok) {
-      throw new Error(`Error request by ${url}, status: ${response.status}`);
+      throw new Error(`Error request by ${queryURL}, status: ${response.status}`);
     }
     const responseJSON = await response.json();
     return responseJSON.data;
@@ -19,7 +20,7 @@ const getRandomCatURL = async () => {
 };
 
 const loadCat = async () => {
-  const date = await getRandomCatURL();
+  const date = await getSearchedGIF();
   console.log(date);
 };
 
